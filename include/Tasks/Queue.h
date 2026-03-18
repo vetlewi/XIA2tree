f1f2b851591f01cd30fda1697262179ba5f5fd98
Vetle W. Ingeberg <vetlewi@fys.uio.no>
2026-03-18 17:15:53 +0100

    Added QueueWorker protector
    I've added a simple QueueWorker protector. The object is handed the output_queue and ensures that the queue is marked as finished when the object is released.

--- include/Tasks/Queue.h
+++ include/Tasks/Queue.h
@@ -15,18 +15,28 @@
 
 #include <blockingconcurrentqueue.h>
 
-//#define SIZE 16384
-#define SIZE 2048
+#define SIZE 16384
+//#define SIZE 2048
 //#define SIZE 32
 
 namespace Task {
+
+    template<typename T>
+    class QueueWorker {
+    private:
+        T& _queue;
+    public:
+        QueueWorker(T& queue) : _queue(queue) {}
+        ~QueueWorker() { _queue.mark_as_finish(); }
+    };
 
     using XIAQueue_t = SPSCBlockingQueue<const XIA_base_t *, SIZE>;
     using EntryQueue_t = SPSCBlockingQueue<Entry_t, SIZE>;
     using EventQueue_t = SPSCBlockingQueue<std::vector<Entry_t>, SIZE>;
     using MCEventQueue_t = SPSCBlockingQueue<std::vector<Entry_t>, SIZE>;
     using TEventQueue_t = SPSCBlockingQueue<std::pair<std::vector<Entry_t>, int>, SIZE>;
-    using MTEventQueue_t = moodycamel::BlockingConcurrentQueue<std::pair<std::vector<Entry_t>, int>>;
+    using MTEventQueue_t = SPSCBlockingQueue<std::pair<std::vector<Entry_t>, int>, SIZE>;
+    //using MTEventQueue_t = moodycamel::BlockingConcurrentQueue<std::pair<std::vector<Entry_t>, int>>;
 }
 
 #endif // QUEUE_H
--- src/Tasks/Calibrator.cpp
+++ src/Tasks/Calibrator.cpp
@@ -18,10 +18,11 @@ Calibrator::Calibrator(OCL::ConfigManager &cal, XIAQueue_t &input, const size_t 
 
 void Calibrator::Run()
 {
+    QueueWorker worker(output_queue);
     const XIA_base_t *xia;
-    //XIA_base_t xia;
     while ( input_queue.is_not_finish() || !input_queue.empty() ) {
         if ( !input_queue.try_pop( xia )) {
+            std::this_thread::yield();
             continue;
         }
 
@@ -31,27 +32,7 @@ void Calibrator::Run()
         }
         Entry_t entry = calibration(xia);
         output_queue.push(entry);
-        /*while ( !output_queue.wait_enqueue_timed(entry, std::chrono::seconds(1)) ){
-            if ( done )
-                break;
-        }*/
     }
     output_queue.mark_as_finish();
-    /*while ( !done ){
-        while ( !input_queue.wait_dequeue_timed(xia, std::chrono::seconds(1)) ){
-            if ( done )
-                break;
-        }
-
-        // Get the entry. If false we can continue.
-        if ( !calibration.keep(xia) ){
-            continue;
-        }
-        Entry_t entry = calibration(xia);
-        while ( !output_queue.wait_enqueue_timed(entry, std::chrono::seconds(1)) ){
-            if ( done )
-                break;
-        }
-    }*/
     is_done = true;
 }
