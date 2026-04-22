f77ba387da185193ce5881f72d190518eb845cbd
Vetle W. Ingeberg <vetlewi@fys.uio.no>
2026-04-22 17:31:53 +0200

    Initial working metadata writer
    This is the most basic writer I have implemented. More can/should be implemented later

--- include/RootInterface/MetadataWriter.h
+++ include/RootInterface/MetadataWriter.h
@@ -7,18 +7,34 @@
 
 #include <string>
 
+#include <TFile.h>
+#include <TDirectory.h>
+
+struct ProcessedDataMetadata {
+    std::map<std::string, size_t> processedData;
+    bool exportData() const;
+};
+
 class MetadataWriter {
 private:
-    static void WriteString(const std::string& key, const std::string& value);
+    TFile file;
+    TDirectory *metaDir;
+
+    ProcessedDataMetadata processedData;
+
+    bool WriteString(const std::string& key, const std::string& value);
 
     template <typename T>
-    static void WriteNumeric(const std::string& key, const T& value);
+    bool WriteNumeric(const std::string& key, const T& value);
 
+    bool WriteSystemInfo();
 
 public:
+    MetadataWriter(const char* fname, const char* folder_name="Metadata", const char* options="UPDATE");
+    ~MetadataWriter();
 
-
-    static bool Write(const char* fname, const char* folder_name="Metadata", const char* options="UPDATE");
+    void AddProcessedMetadata(const std::map<std::string, size_t>& map){ processedData = {map};}
+    bool Write();
 
 };
 
--- src/RootInterface/MetadataWriter.cpp
+++ src/RootInterface/MetadataWriter.cpp
@@ -6,31 +6,78 @@
 #include <TNamed.h>
 #include <TParameter.h>
 
+#include <string>
+
+#include <hwinfo/hwinfo.h>
+#include <hwinfo/utils/unit.h>
+
 #include "MetadataWriter.h"
 
-void MetadataWriter::WriteString(const std::string &key, const std::string &value) {
+bool ProcessedDataMetadata::exportData() const {
+    // Total number of events read from file
+    if ( processedData.empty() )
+        return false;
+    //auto events_found = processedData[std::string("XIAReader")];
+    TParameter events_found("RawEntries", static_cast<int64_t>(processedData.at("XIAReader")));
+    if (events_found.Write() == 0) return false;
+
+    TParameter entriesDropped("DroppedEvents", static_cast<int64_t>(processedData.at("Buffer")) - static_cast<int64_t>(processedData.at("Calibrator")));
+    if (entriesDropped.Write() == 0) return false;
+
+    TParameter eventsFound("EventsFound",  static_cast<int64_t>(processedData.at("Trigger")));
+    if (eventsFound.Write() == 0) return false;
+
+    TParameter triggeredEventsFound("TriggeredEvents", static_cast<int64_t>(processedData.at("Sorter")));
+    if (triggeredEventsFound.Write() == 0) return false;
+
+    return true;
+}
+
+bool MetadataWriter::WriteString(const std::string &key, const std::string &value) {
     TNamed obj(key.c_str(), value.c_str());
-    obj.Write();
+    return (obj.Write() > 0);
 }
 
 template<typename T>
-void MetadataWriter::WriteNumeric(const std::string &key, const T &value) {
+bool MetadataWriter::WriteNumeric(const std::string &key, const T &value) {
     TParameter param(key.c_str(), value);
-    param.Write();
+    return (param.Write() > 0);
 }
 
-bool MetadataWriter::Write(const char* fname, const char* folder_name, const char* options) {
-    TFile file(fname, options);
-    auto metaDir = file.GetDirectory(folder_name);
+MetadataWriter::MetadataWriter(const char* fname, const char* folder_name, const char* options)
+    : file(fname, options)
+    , metaDir( file.GetDirectory(folder_name) )
+{
     if ( !metaDir ) {
         metaDir = file.mkdir(folder_name);
     }
-    metaDir->cd();
+}
 
-    // Now we can start writing to file...
+bool MetadataWriter::WriteSystemInfo() {
+    auto cpus = hwinfo::getAllCPUs();
+    WriteString("CPU.vendor", cpus[0].vendor());
+    WriteString("CPU.model", cpus[0].modelName());
+    WriteNumeric("CPU.physicalCores", cpus[0].numPhysicalCores());
+    WriteNumeric("CPU.logicalCores", cpus[0].numLogicalCores());
+    WriteNumeric("CPU.frequency", cpus[0].regularClockSpeed_MHz());
 
+    hwinfo::Memory memory;
+    WriteNumeric("Memory.totalSize", memory.total_Bytes());
 
-    file.Write();
+    hwinfo::OS os;
+    WriteString("OS.name", os.name());
+    WriteString("OS.kernel", os.kernel());
+    WriteString("OS.version", os.version());
+    WriteString("OS.endian", os)
+}
+
+MetadataWriter::~MetadataWriter() {
+    Write();
     file.Close();
-    return true;
+}
+
+bool MetadataWriter::Write() {
+    metaDir->cd();
+    if (!processedData.exportData()) return false;
+    return (file.Write() > 0);
 }
